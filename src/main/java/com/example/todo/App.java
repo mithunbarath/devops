1190e1665ff96dc11b080cb0125701ac38c93e01
mithunbarath <127648515+mithunbarath@users.noreply.github.com>
2025-07-22 22:21:50 +0530

    Update App.java

--- src/main/java/com/example/todo/App.java
+++ src/main/java/com/example/todo/App.java
@@ -3,5 +3,10 @@ package com.example.todo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven, Git, GitHub, Docker, and Jenkins!");
+        int a=7;
+        int b=8;
+        int c=a+b;
+        System.out.println(c);
+
     }
 }
